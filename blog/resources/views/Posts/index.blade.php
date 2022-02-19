dd8842a9ae6f44e579c36602a3b58f3830459025
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-02-19 15:38:08 +0200

    preparing the routes for the post resorce

---
+++ blog/resources/views/Posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="test-center">
+        <a href="posts.create" class="btn btn-sucsess">
+            Create Post
+        </a>
+    </div>
+
+@endsection
--- blog/resources/views/layouts/main.blade.php
+++ blog/resources/views/layouts/main.blade.php
@@ -8,7 +8,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    @extends('layouts.nav')
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ITI-blog-page</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">All Posts</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container">
         @yield('content')
     </div>
