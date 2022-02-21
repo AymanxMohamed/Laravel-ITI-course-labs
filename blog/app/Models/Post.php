b793b994b5deea9001fb9a8289d49cec6db49764
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-02-21 15:01:52 +0200

    requiring slugs and add new migration for adding the slug column, make the post sluggable

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,11 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Carbon\Carbon;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
     protected $fillable = [
         'user_id',
